e12785cf901d694725772c9dff915d8f522b583e
khristop <kevinchristo_garcia@hotmail.com>
2016-05-10 16:18:55 -0600

    semana 4 2

--- semana 4/Prestamos/apps/ControlPrestamo/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from models import *
-
-# Register your models here.
- 
-admin.site.register(TipoArticulo)
-admin.site.register(Articulo)
-admin.site.register(Estudiante)
-admin.site.register(DetallePrestamo)
